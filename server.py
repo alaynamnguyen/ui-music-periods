ce6e989241d0f04d258d4e08ac1049bbc339ecda
Alayna Nguyen <32422836+alaynamnguyen@users.noreply.github.com>
2024-04-14 16:14:12 -0400

    Added homepage

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import render_template
+from flask import Response, request, jsonify, redirect, url_for
+import json
+app = Flask(__name__)
+
+current_id = 2
+data = json.load(open('data.json'))
+
+# ROUTES
+
+@app.route('/')
+def home():
+   return render_template('home.html', data=data)   
+
+
+@app.route('/view/<id>')
+def view(id=None):
+    return render_template('view.html', id=id, data=data) 
+
+if __name__ == '__main__':
+   app.run(debug = True)
+
+
+
+
