052ab260d56285d6d2a9ea52c925c687e7fdc43e
Alayna Nguyen <32422836+alaynamnguyen@users.noreply.github.com>
2024-05-05 14:21:16 -0400

    Made the music period boxes on the learn page clickable

--- static/home.js
+++ static/home.js
@@ -1,10 +1,28 @@
 $(document).ready(function () {
-  $("#startButton").click(function () {
-    event.preventDefault();
-    window.location.href = "/learn/1";
-  });
-  $("#quizButton").click(function () {
-    event.preventDefault();
-    window.location.href = "/restart_quiz";
-  });
+    $("#startButton").click(function () {
+        event.preventDefault();
+        window.location.href = "/learn/1";
+    });
+    $("#quizButton").click(function () {
+        event.preventDefault();
+        window.location.href = "/restart_quiz";
+    });
+    $("#medieval").click(function () {
+        window.location.href = "/learn/1";
+    });
+    $("#baroque").click(function () {
+        window.location.href = "/learn/3";
+    });
+    $("#romantic").click(function () {
+        window.location.href = "/learn/5";
+    });
+    $("#renaissance").click(function () {
+        window.location.href = "/learn/2";
+    });
+    $("#classical").click(function () {
+        window.location.href = "/learn/4";
+    });
+    $("#century").click(function () {
+        window.location.href = "/learn/6";
+    });
 });
